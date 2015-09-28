cc647fa7e5050014dbb8f11dc17a9a1f1095fa29
huangjin <1228735902@qq.com>
2015-09-28 21:02:28 +0800

    hello,i will commit

--- hello.php
+++ hello.php
@@ -1,3 +1,10 @@
 <?php
 echo "Hello";
+echo "";
 ?>
+获取hj.git到本地
+git clone https://github.com/zctyhj/hj.git
+设置user
+git config --global user.name "Your Name"
+git config --global user.email you@example.com
+
