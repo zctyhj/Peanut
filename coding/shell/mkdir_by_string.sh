5624be96f71ab10b6f561ae3ebb24bc6492aa419
Hunter Huang <1228735902@qq.com>
2018-08-31 17:54:54 +0800

    rename
    rename

---
+++ coding/shell/mkdir_by_string.sh
@@ -0,0 +1,37 @@
+﻿#
+#  directory: D:\Hunter\GitHub\Peanut\coding\shell
+#  fineName : mkdir_by_string.sh
+#  function : 通过写入的文件夹的内容，批量创建文件夹
+#  author   : hunter huang
+#  Date     : 2018年8月31日
+#  tips     : 使用者确定文件夹配置文件完全正确默认完全符合要求
+#
+
+set filepath =D:\Hunter\GitHub\Peanut\coding\shell\file.txt
+
+#思路：按行读取文件，需要指定的信息越少越好，当然还要分层级，树形模型来分别创建处理
+#使用缩略空格 表示层级关系，默认处理全是文件夹的情况
+#获取某个路径下的文件名
+
+两种模式
+1. 都是文件夹 -d
+2. 有文件夹有文件 -c
+
+
+#读取文件
+
+
+#创建文件夹
+
+
+#检查并返回
+
+html
+    font
+    admin
+css
+js
+index.html
+
+
+#需要考虑问题，变量如何使用  文件如何读写  循环如何使用
